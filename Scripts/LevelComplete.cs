3452db570faf70e70b863df49ed0d5559cb2255f
Florgol <38015664+Florgol@users.noreply.github.com>
2020-11-16 12:40:03 +0100

    Add files via upload

---
+++ Scripts/LevelComplete.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelComplete : MonoBehaviour
+{
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadPreviousLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void LoadPreviousPreviousLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    public float upwardForce = 1000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    // FixedUpdate is used for physics stuff
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        //moving upward
+
+        if (rb.position.y > 0.5f && rb.position.y < 1.5f && rb.position.x < 7.5f && rb.position.x > -7.5f)
+        {
+            if (Input.GetKey("w"))
+            {
+                rb.AddForce(0, upwardForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+            }
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
